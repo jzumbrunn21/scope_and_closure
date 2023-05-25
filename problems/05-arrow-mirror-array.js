cb99fae9e648ce1c0969625f2b8bab27712f0033
Josh <jzumbrunn@yahoo.com>
2023-05-25 18:14:35 -0400

    problems

--- problems/05-arrow-mirror-array.js
+++ problems/05-arrow-mirror-array.js
@@ -18,8 +18,8 @@ const arrowMirrorArray = (array) => {
   return array
 }
 
-console.log(arrowMirrorArray([1,2,3])); // => [ 1, 2, 3, 3, 2, 1 ]
-console.log(arrowMirrorArray(['a', 'b', 'c', 'd'])); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
+// console.log(arrowMirrorArray([1,2,3])); // => [ 1, 2, 3, 3, 2, 1 ]
+// console.log(arrowMirrorArray(['a', 'b', 'c', 'd'])); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
--- problems/06-closure-sandwiches.js
+++ problems/06-closure-sandwiches.js
@@ -17,8 +17,23 @@ sandwich2("pb") // => "One sandwich with tomato and pb"
 
 ***********************************************************************/
 
-// Your code here
+function sandwichMaker() {
+  let order = "One sandwich with tomato"
 
+  return function sandwich(str){
+    return order += " and " + str;
+  }
+}
+
+
+// let sandwich = sandwichMaker(); // => returns a function
+// console.log(sandwich("spinach")) // => "One sandwich with tomato and spinach"
+// console.log(sandwich("jelly")) // => "One sandwich with tomato and spinach and jelly"
+// console.log(sandwich("bread"))// => "One sandwich with tomato and spinach and jelly and bread"
+
+// // Another Sandwich:
+// let sandwich2 = sandwichMaker(); // => returns a function
+// console.log(sandwich2("pb")) // => "One sandwich with tomato and pb"
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
