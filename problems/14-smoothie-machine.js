71d9281812bfa25b5cf239d29973e066fc016459
edmundtetteh <edmundtetteh2000@hotmail.com>
2023-05-29 16:04:10 -0600

    problem 14 solved

--- problems/14-smoothie-machine.js
+++ problems/14-smoothie-machine.js
@@ -24,6 +24,33 @@ console.log(smoothie2("pineapple"));
 
 // Your code here
 
+function smoothieMachine(...newpara) {
+   let str1 = "I'm having a smoothie with" 
+   
+  return function(...para){
+    for( let word of para){
+      newpara.push(word)
+    }
+    return str1 + ' ' + newpara.join(' and ')
+  }
+  
+  
+}
+
+
+let smoothie1 = smoothieMachine();
+
+console.log(smoothie1("milk"));
+// prints "I'm having a smoothie with milk"
+console.log(smoothie1("kale", "spinach"));
+// prints "I'm having a smoothie with milk and kale and spinach"
+console.log(smoothie1("honey", "pears", "berries"));
+// prints "I'm having a smoothie with milk and kale and spinach and honey and pears and berries"
+
+// let smoothie2 = smoothieMachine("apples", "bananas", "berries");
+// console.log(smoothie2("pineapple"));
+// prints "I'm having a smoothie with apples and bananas and berries and pineapple"
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = smoothieMachine;
