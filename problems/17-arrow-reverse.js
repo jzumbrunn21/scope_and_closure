68530ca4da824e928186dc110269626a4ad80e4d
edmundtetteh <edmundtetteh2000@hotmail.com>
2023-05-29 18:39:10 -0600

    problem 16 solved

--- problems/17-arrow-reverse.js
+++ problems/17-arrow-reverse.js
@@ -13,6 +13,14 @@ let result3 = arrowReverseString("potato"); // returns "otatop"
 ***********************************************************************/
 
 // Your code here
+const arrowReverseString = str => str.split('').reverse().join('')
+
+let result1 = arrowReverseString("hello"); // returns "olleh"
+console.log(result1)
+let result2 = arrowReverseString("garden"); // returns "nedrag"
+console.log(result2)
+let result3 = arrowReverseString("potato"); // returns "otatop"
+console.log(result3)
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 
