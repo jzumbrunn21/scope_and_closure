9c115024e0f541da58afdb7737129d243c8ff1b8
edmundtetteh <edmundtetteh2000@hotmail.com>
2023-05-29 19:02:48 -0600

    problem 19 solved

--- problems/19-coin-collector.js
+++ problems/19-coin-collector.js
@@ -27,6 +27,38 @@ Example 3:
 
 // Your code here
 
+function coinCollector(numCoins) {
+  let newArr = []
+
+
+  let coins = function(num){
+    if(newArr.length <= numCoins){
+      newArr.push(num)
+      return newArr
+    }else{
+      return coins
+    }
+  }
+    return coins
+  }
+  
+
+
+
+  let oneCoin = coinCollector(1); // returns a function
+  console.log(oneCoin(10)); // prints [10]
+
+
+  let twoCoins = coinCollector(2); // returns a function
+  twoCoins(25); // returns a function
+  console.log(twoCoins(10)); // prints [25, 10]
+
+
+  let threeCoins = coinCollector(3); // returns a function
+  threeCoins(25); // returns a function
+  threeCoins(5); // returns a function
+  console.log(threeCoins(10)); // prints [ 25, 5, 10 ]
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
